b1edf64d975b0726e4995dae1f473bf3d0a11282
My Name <my-name@chromium.org>
2022-02-01 13:48:10 +0100

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstWPF_App
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            double ActHeight;
+            if (button1.IsMouseOver)
+            {
+                ActHeight = button1.ActualHeight;
+                textbox1.Text = ActHeight.ToString();
+            }
+            else {
+                textbox1.Text = "";
+            }
+        }
+    }
+}
